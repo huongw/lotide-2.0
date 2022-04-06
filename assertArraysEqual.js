ba64563651a49645885d91a658323946e3afc0c7
Whitney <designedbywh@gmail.com>
2022-04-06 11:04:20 -0400

    fix string in function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,7 +16,7 @@ const assertArraysEqual = function(arr1, arr2) {
   const isTrue = eqArrays(arr1, arr2);
 
   if (isTrue) {
-    console.log(`✅ Assertion Passed: ${arr1} !== ${arr2}`);
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
   }
