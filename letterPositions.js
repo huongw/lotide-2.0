34b94a16eb4ef035730591410b38e7ba399e2bce
Whitney <designedbywh@gmail.com>
2022-04-28 19:55:47 -0400

    implement eqObjects function and fix for loop

--- letterPositions.js
+++ letterPositions.js
@@ -3,7 +3,7 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   
-  for (let i = 1; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
